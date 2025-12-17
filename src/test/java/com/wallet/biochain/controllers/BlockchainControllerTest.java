95ad2fbc9d6b6b48fa5500db2a3bf8cf5b49b2eb
Mousaab <mousaab.ougrich@emsi-edu.ma>
2025-12-17 15:21:16 +0100

    Replace deprecated @MockBean with @MockitoBean in controller tests

--- src/test/java/com/wallet/biochain/controllers/BlockchainControllerTest.java
+++ src/test/java/com/wallet/biochain/controllers/BlockchainControllerTest.java
@@ -9,7 +9,7 @@ import com.wallet.biochain.services.BlockchainService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -26,10 +26,10 @@ class BlockchainControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private BlockchainService blockchainService;
 
-    @MockBean
+    @MockitoBean
     private BlockService blockService;
 
     @Test
--- src/test/java/com/wallet/biochain/controllers/NodeControllerTest.java
+++ src/test/java/com/wallet/biochain/controllers/NodeControllerTest.java
@@ -9,7 +9,7 @@ import com.wallet.biochain.services.NodeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,7 +27,7 @@ class NodeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private NodeService nodeService;
 
     @Test
